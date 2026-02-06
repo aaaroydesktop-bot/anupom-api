92a3d33d326f277f6e9183b315f8529fc2aae7b5
Anupom <anupom@gmail.com>
2026-02-06 11:52:11 +0530

    update api endpoints

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Question(BaseModel):
+    question: str
 
 @app.get("/")
 def home():
     return {
         "status": "running",
         "model": "Anupom",
-        "created_by": "Anupom"
+        "version": "v2"
+    }
+
+@app.post("/ask")
+def ask(data: Question):
+    return {
+        "answer": f"You asked: {data.question}"
     }
